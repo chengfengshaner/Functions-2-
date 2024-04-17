b63e67e53d19aaf07fbc4e53b2a5506d3897f933
chengfengshaner <157273073+chengfengshaner@users.noreply.github.com>
2024-04-17 21:24:27 +0800

    Add files via upload

---
+++ 函数的调用（2）/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//一个函数如果不写返回类型，默认返回int类型
+int is_leap_year(int n)
+{
+	if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0))
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int y = 0;
+	for (y = 1000; y <= 2000; y++)
+	{
+		if (is_leap_year(y) == 1)
+		{
+			printf("%d ", y);
+		}
+	}
+	return 0;
+}
